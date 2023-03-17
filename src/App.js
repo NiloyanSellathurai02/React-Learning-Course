4155af324f6d34a672e641e8449fe4790399d8c7
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-03-17 22:44:46 +0100

    learned about UserInputs with forms

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = () => {
   const data = [
     {
@@ -20,7 +21,7 @@ const App = () => {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={data} />
     </div>
   );
