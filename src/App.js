04dc5fb9902fa14e1e88132be632cedcb0a31fed
NiloyanSellathurai02 <n.sella2001@gmail.com>
2023-03-17 02:43:42 +0100

    Did learned about components and props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import Expenses from "./components/Expenses";
+
 function App() {
+  const data = [
+    {
+      title: "Audi",
+      amount: 398.99,
+      date: new Date(2022, 2, 29),
+    },
+    {
+      title: "Centraal Beheer",
+      amount: 88.75,
+      date: new Date(2022, 2, 31),
+    },
+    {
+      title: "Belastingdienst",
+      amount: 79.0,
+      date: new Date(2022, 3, 1),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={data} />
     </div>
   );
 }
